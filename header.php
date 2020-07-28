7b3f43a5a49886a2fc77b6bab24687e236a710d1
h.yokoyama <haruhi.yokoyama@wills.co.jp>
2020-07-28 15:03:59 +0900

    headerにコメントを追加

--- header.php
+++ header.php
@@ -56,6 +56,7 @@
 		<?php if ( get_header_image() ) : ?>
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
 		<?php endif; ?>
+		コメント！
 	</header><!-- #masthead -->
 
 	<div id="main" class="wrapper">
