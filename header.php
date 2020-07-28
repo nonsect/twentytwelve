db019a914c55f8649a4d199421d8ea0bef51fe6c
h.yokoyama <haruhi.yokoyama@wills.co.jp>
2020-07-28 15:20:03 +0900

    headerのコメントの位置を変更

--- header.php
+++ header.php
@@ -35,6 +35,7 @@
 <?php wp_body_open(); ?>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
+	コメント！
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
@@ -56,7 +57,6 @@
 		<?php if ( get_header_image() ) : ?>
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
 		<?php endif; ?>
-		コメント！
 	</header><!-- #masthead -->
 
 	<div id="main" class="wrapper">
